fcec4507678e50fd1bb51d94e940e892a53ff2da
Hamish Johnson <hamish@magpiedigital.org>
2018-03-09 20:08:00 +0000

    Understanding Enums

---
+++ Swift/Enums, Structs and Classes.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+enum CSSColor {
+    enum ColorName: String {
+        case black, silver, gray, white, maroon, red, purple, fuchsia, green, lime, olive, yellow, navy, blue, teal, aqua
+    }
+    
+    case named(ColorName)
+    case rgb(UInt8, UInt8, UInt8)
+}
+
+extension CSSColor: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .named(let colorName):
+            return colorName.rawValue
+        case .rgb(let red, let green, let blue):
+            return String(format: "#%02X%02X%02X", red,green,blue)
+        }
+    }
+}
+
+let color1 = CSSColor.named(.red)
+let color2 = CSSColor.rgb(0xAA, 0xAA, 0xAA)
+print("color1 = \(color1), color2 = \(color2)") // prints color1 = red, color2 = #AAAAAA
+
+extension CSSColor {
+    init(gray: UInt8) {
+        self = .rgb(gray, gray, gray)
+    }
+}
+
+let color3 = CSSColor(gray: 0xaa)
+print(color3)  //  prints #AAAAAA
+
+CSSColor.named(.blue)
+
+enum Math {
+    static let phi = 1.6180339887498948482 // golden mean
+}
